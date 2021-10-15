895bb1aae10d1dcad9bf306d520202e677bd29f9
cyh <cccchenyuhao@gmail.com>
2021-10-15 17:38:01 +0800

    update README

--- client/src/app.js
+++ client/src/app.js
@@ -1,17 +1,17 @@
-import { Dialog } from '@reach/dialog';
-import Logo from './components/logo';
+import { Dialog } from "@reach/dialog";
+import Logo from "./components/logo";
 
-import '@reach/dialog/styles.css';
-import { useState } from 'react';
+import "@reach/dialog/styles.css";
+import { useState } from "react";
 
 function LoginForm({ onSubmit, buttonText }) {
   const handleSubmit = (e) => {
     e.preventDefault();
-    const username = e.target.elements.username.value;
-    const password = e.target.elements.password.value;
-    console.log(username, password);
-    // onSubmit({ username, password });
+    const username = e.target.elements?.username?.value;
+    const password = e.target.elements?.password?.value;
+    onSubmit({ username, password });
   };
+
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       <div>
@@ -30,24 +30,35 @@ function LoginForm({ onSubmit, buttonText }) {
 }
 
 export default function App() {
-  const [openModal, setOpenModal] = useState('none');
+  const [openModal, setOpenModal] = useState("none");
+
+  const login = (formData) => {
+    console.log("login", formData);
+  };
+
+  const register = (formData) => {
+    console.log("register", formData);
+  };
+
   return (
     <div>
       <Logo />
       <h1>bookshelf</h1>
-      <button onClick={() => setOpenModal('login')}>Login</button>
-      <button onClick={() => setOpenModal('register')}>register</button>
+      <button onClick={() => setOpenModal("login")}>Login</button>
+      <button onClick={() => setOpenModal("register")}>register</button>
       <Dialog
         aria-label="login"
-        isOpen={openModal === 'login'}
-        onDismiss={() => setOpenModal('none')}
-      >
-        <button onClick={() => setOpenModal('none')}>Close</button>
-        <LoginForm buttonText="login" />
+        isOpen={openModal === "login"}
+        onDismiss={() => setOpenModal("none")}>
+        <button onClick={() => setOpenModal("none")}>Close</button>
+        <LoginForm buttonText="login" onSubmit={login} />
       </Dialog>
-      <Dialog isOpen={openModal === 'register'} aria-label="register">
-        <button onClick={() => setOpenModal('none')}>Close</button>
-        <LoginForm buttonText="register" />
+      <Dialog
+        isOpen={openModal === "register"}
+        aria-label="register"
+        onDismiss={() => setOpenModal("none")}>
+        <button onClick={() => setOpenModal("none")}>Close</button>
+        <LoginForm buttonText="register" onSubmit={register} />
       </Dialog>
     </div>
   );
