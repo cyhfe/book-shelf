950a9e114e70ae8f7f739501aa5a387a9a53949d
chenyuhao <cccchenyuhao@gmail.com>
2021-10-15 13:04:27 +0800

    port

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # book-shelf
+
+## deploy
+
+```bash
+heroku login -i
+
+heroku create cyh-bookshelf
+
+git subtree push --prefix server heroku main
+```
--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "../public")));
+const PORT = process.env.PORT || 3000;
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../public/index.html"));
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
-app.listen(3000, () => {
-  console.log("server running in " + 3000);
+app.listen(PORT, () => {
+  console.log('server running in ' + PORT);
 });
 
 module.exports = app;
